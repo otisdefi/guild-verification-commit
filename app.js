34c6aa7951c3fd0e44595b41c46eba507845c368
otisdefi <116648005+otisdefi@users.noreply.github.com>
2026-02-21 01:22:22 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-20T22:22:13.907Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-02-20T22:22:21.725Z
+Commit: Fix CSS styling bug on mobile devices
