0fc72bf61774fcfcad6a43da7c677b02dad204ed
otisdefi <116648005+otisdefi@users.noreply.github.com>
2026-02-21 01:23:05 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-20T22:22:54.612Z
-Commit: Add multi-language support
+Timestamp: 2026-02-20T22:23:05.579Z
+Commit: Resolve timezone issues in date handling
