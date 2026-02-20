7cfce6f17e0acc86781e105648b51ea8847b30d8
otisdefi <116648005+otisdefi@users.noreply.github.com>
2026-02-21 01:22:17 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-20T22:21:45.855Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-02-20T22:22:17.253Z
+Commit: Resolve null pointer exception in user service
