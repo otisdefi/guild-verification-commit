5e3fd164d1468d571a36eebc3185be6b106a5976
otisdefi <116648005+otisdefi@users.noreply.github.com>
2026-02-21 01:22:54 +0300

    Add multi-language support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-20T22:22:34.046Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-02-20T22:22:54.612Z
+Commit: Add multi-language support
