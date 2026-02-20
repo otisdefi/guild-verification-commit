7c3b671c940a19299e548ca4ee841426a7358306
otisdefi <116648005+otisdefi@users.noreply.github.com>
2026-02-20 22:01:02 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-20T19:00:44.457Z
-Commit: Add export functionality for reports
+Timestamp: 2026-02-20T19:01:02.069Z
+Commit: Patch security vulnerability in authentication
