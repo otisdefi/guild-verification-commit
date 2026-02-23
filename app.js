9a324fbb471bea71506679223b989f9ecb34f264
otisdefi <116648005+otisdefi@users.noreply.github.com>
2026-02-23 14:02:31 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-20T22:22:55.918Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-02-23T11:02:30.792Z
+Commit: Resolve null pointer exception in user service
