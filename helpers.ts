72c832db0ab4e904225e8cbcda54b68ea5d657f5
otisdefi <116648005+otisdefi@users.noreply.github.com>
2026-02-28 21:20:35 +0300

    Fix race condition in async operations

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-28T18:20:33.219Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-02-28T18:20:34.883Z
+Commit: Fix race condition in async operations
