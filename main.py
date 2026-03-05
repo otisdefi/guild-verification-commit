7578e50d0f5fd8d01583298e51765fde5d3f9164
otisdefi <116648005+otisdefi@users.noreply.github.com>
2026-03-05 23:00:09 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T20:00:02.973Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T20:00:09.009Z
+Commit: Fix broken links in documentation
